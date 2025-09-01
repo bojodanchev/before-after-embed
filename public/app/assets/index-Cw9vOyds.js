88bff30778e5ba34632afa31768b6ee829e5724a
bojodanchev <bojodanchev@gmail.com>
2025-09-01 12:14:16 +0300

    ux(portal): collapse secondary embed actions under More; remove Open

--- public/app/assets/index-Cw9vOyds.js
+++ public/app/assets/index-Cw9vOyds.js
@@ -1,4 +1,4 @@
-import{r as h,j as e,c as q,R}from"./index-C9Ewpt7r.js";/**
+import{r as h,j as e,c as q,R}from"./index-Cr87LODh.js";/**
  * @license lucide-react v0.542.0 - ISC
  *
  * This source code is licensed under the ISC license.
